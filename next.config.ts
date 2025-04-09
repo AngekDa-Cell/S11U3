b5cc4cdc8619a8e7d84d4d64467084e520a9f18c
AngekDa-Cell <angeldavidpadillachavez@gmail.com>
2025-04-09 00:52:09 -0600

    FIN

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,15 @@
-import { nextConfig } from "./lib/utils";
+import type { NextConfig } from "next";
+
+// next.config.ts
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    domains: ["localhost"],
+  }
+};
 
 export default nextConfig;
 
